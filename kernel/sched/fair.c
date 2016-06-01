d8033e843428ee78bb1a50bb03cb23d7b005b402
paloda <paloda09@gmail.com>
2016-06-01 22:27:12 +0200

    sched: Optimize the select_best_cpu() "for" loop
    select_best_cpu() is agnostic of the hardware topology. This means that
    certain functions such as task_will_fit() and skip_cpu() are run
    unnecessarily for every CPU in a cluster whereas they need to run only
    once per cluster. Reduce the execution time of select_best_cpu() by
    ensuring these functions run only once per cluster. The frequency domain
    mask is used to identify CPUs that fall in the same cluster.
    
    CRs-fixed: 849655
    Change-Id: Id24208710a0fc6321e24d9a773f00be9312b75de
    Signed-off-by: Syed Rameez Mustafa <rameezmustafa@codeaurora.org>
    [joonwoop@codeaurora.org: ported from msm-3.10]
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>
    
    (cherry picked from commit f2948d23a469ce44abe8ba41f508b1883c44426f)

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1901,9 +1901,7 @@ static int best_small_task_cpu(struct task_struct *p, int sync)
 
 static int skip_freq_domain(struct rq *task_rq, struct rq * rq, int reason)
 {
-	struct rq *rq = cpu_rq(cpu);
-	struct rq *task_rq = task_rq(p);
-	int skip;
+        int skip;
 
         if (!reason)
                 return 0;
@@ -1918,8 +1916,7 @@ static int skip_freq_domain(struct rq *task_rq, struct rq * rq, int reason)
 		break;
 
 	default:
-		skip = (rq == task_rq);
-		break;
+		return 0;
 	}
 
         return skip;
@@ -1976,7 +1973,7 @@ static int select_packing_target(struct task_struct *p, int best_cpu)
 static int select_best_cpu(struct task_struct *p, int target, int reason,
 			   int sync)
 {
-	int i, best_cpu = -1, fallback_idle_cpu = -1, min_cstate_cpu = -1;
+	int i, j, best_cpu = -1, fallback_idle_cpu = -1, min_cstate_cpu = -1;
 	int cpu_cost, min_cost = INT_MAX;
 	int min_idle_cost = INT_MAX, min_busy_cost = INT_MAX;
 	u64 tload, cpu_load;
@@ -2017,27 +2014,29 @@ static int select_best_cpu(struct task_struct *p, int target, int reason,
 			continue;
 		}
 
-	/* Todo : Optimize this loop */
-	for_each_cpu_and(i, tsk_cpus_allowed(p), cpu_online_mask) {
-		if (skip_cpu(p, i, reason))
+		if (skip_cpu(trq, rq, i, reason))
 			continue;
-
-		tload = scale_load_to_cpu(task_load(p), i);
-		cpu_load = cpu_load_sync(i, sync);
-		trace_sched_cpu_load(cpu_rq(i), idle_cpu(i),
-				     mostly_idle_cpu_sync(i, cpu_load, sync),
-				     power_cost(tload, i));
 
 		/*
 		 * The least-loaded mostly-idle CPU where the task
 		 * won't fit is our fallback if we can't find a CPU
 		 * where the task will fit.
 		 */
+		tload = scale_load_to_cpu(task_load(p), i);
+		trace_sched_cpu_load(cpu_rq(i), idle_cpu(i),
+				     mostly_idle_cpu_sync(i,
+							 cpu_load_sync(i, sync),
+							 sync),
+				     power_cost(tload, i));
 		if (!task_load_will_fit(p, tload, i)) {
-			if (mostly_idle_cpu_sync(i, cpu_load, sync)) {
-				if (cpu_load < min_fallback_load) {
-					min_fallback_load = cpu_load;
-					fallback_idle_cpu = i;
+			for_each_cpu_and(j, &search_cpus,
+						&rq->freq_domain_cpumask) {
+				cpu_load = cpu_load_sync(j, sync);
+				if (mostly_idle_cpu_sync(j, cpu_load, sync)) {
+					if (cpu_load < min_fallback_load) {
+						min_fallback_load = cpu_load;
+						fallback_idle_cpu = j;
+					}
 				}
 			}
 			cpumask_andnot(&search_cpus, &search_cpus,
@@ -2045,6 +2044,7 @@ static int select_best_cpu(struct task_struct *p, int target, int reason,
 			continue;
 		}
 
+		cpu_load = cpu_load_sync(i, sync);
 		if (!eligible_cpu(tload, cpu_load, i, sync))
 			continue;
 
